ead9b328f24e41bd1d1f8eb3cf86dc65496ea28d
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2021-11-04 20:10:03 +0000

    push before delete

--- src/main/resources/puppies-data.sql
+++ src/main/resources/puppies-data.sql
@@ -1,2 +1,8 @@
-INSERT INTO `puppies`(`age`,`breed`,`name`) VALUES(4,'Chihuahua','Peppy');
+INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(4,'Chihuahua','Peppy');
+INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(3,'Chihuahua','Niko');
+INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(13,'Jack Russell','Lucky');
+INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(15,'Staff','Dollar');
+INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(9,'Spaniel','Rosie');
+INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(100,'Husky','Tiger');
+
 -- H2 databases REQUIRE ''(single quotes) for strings
