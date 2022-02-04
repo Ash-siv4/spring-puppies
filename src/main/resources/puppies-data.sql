e9ca79cebcff66d7fc4468ed65fa80095298526b
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-02-04 12:39:59 +0000

    testing bug fix - data.sql file updated

--- src/main/resources/puppies-data.sql
+++ src/main/resources/puppies-data.sql
@@ -1,8 +1,3 @@
-INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(4,'Chihuahua','Peppy');
-INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(3,'Chihuahua','Niko');
-INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(13,'Jack Russell','Lucky');
-INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(15,'Staff','Dollar');
-INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(9,'Spaniel','Rosie');
-INSERT INTO `puppies`.`puppies`(`age`,`breed`,`name`) VALUES(100,'Husky','Tiger');
+INSERT INTO `puppies`(`age`,`breed`,`name`) VALUES(4,'Chihuahua','Peppy');
 
 -- H2 databases REQUIRE ''(single quotes) for strings
